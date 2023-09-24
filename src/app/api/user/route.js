704bd613120b85846246c54f3096a316778bd15a
Mannan <abdul.mannan@techexons.com>
2023-09-24 19:16:01 +0500

    .

--- src/app/api/user/route.js
+++ src/app/api/user/route.js
@@ -1,11 +1,11 @@
-import { NextResponse } from "next/server";
-import User from "../../../../model/user";
-import { mongoDBConnection } from "../../../lib/mongodb";
+// import { NextResponse } from "next/server";
+// import User from "../../../../model/user";
+// import { mongoDBConnection } from "../../../lib/mongodb";
 
-export async function POST(request) {
-  const { email, password } = await request.json();
-  console.log("Doneeeee-----------------");
-  await mongoDBConnection();
-  await User.create({ email, password });
-  return NextResponse.json({ message: "User registered!" }, { status: 200 });
-}
+// export async function POST(request) {
+//   const { email, password } = await request.json();
+//   console.log("Doneeeee-----------------");
+//   await mongoDBConnection();
+//   await User.create({ email, password });
+//   return NextResponse.json({ message: "User registered!" }, { status: 200 });
+// }
--- src/lib/auth.js
+++ src/lib/auth.js
@@ -1,5 +1,5 @@
-import GoogleProvider from "next-auth/providers/google";
-import GithubProvider from "next-auth/providers/github";
+// import GoogleProvider from "next-auth/providers/google";
+// import GithubProvider from "next-auth/providers/github";
 import CredentialsProvider from "next-auth/providers/credentials";
 
 export const authOptions = {
